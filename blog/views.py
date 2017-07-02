4f946d07e8d26976c3fdcd3a4eda2529b773b8ac
Nora Fergany <naf29@pitt.edu>
2017-07-02 13:21:17 -0400

    my changes

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now())
+    posts = Post.objects.filter(published_date__isnull=False).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 
