7c4f82d02fc572b9605360cce588913412118a2a
Nora Fergany <naf29@pitt.edu>
2020-07-20 13:16:07 -0400

    Fix URL routing

--- blog/urls.py
+++ blog/urls.py
@@ -4,8 +4,8 @@ from django.contrib import admin
 
 urlpatterns = [
     # url(r'^admin/', admin.site.urls),
-    url(r'^posts$', views.post_list, name='post_list'),
-    url(r'^$', views.home, name='home'),
+    url(r'^posts/$', views.post_list, name='post_list'),
+    url(r'^$', views.index, name='index'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
